16ba020900af24f8b27266c492e839d4f96faa7c
Yosbel Esperón González <yosbeleg89@live.com>
2019-05-22 10:07:02 -0400

    add AppTest solution

---
+++ AppTest/Controllers/NegotiationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppTest.Controllers
+{
+    [ApiController]
+    public class NegotiationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("about")]
+        //GET help
+        public string Get()
+        {
+            return "Send Post with an Aidbox Operation resource in the body";
+        }
+
+        // POST negotiate-mdtb-link
+        [Route("negotiate-mdtb-link")]
+        [HttpPost]
+        public string Post(object aidboxRequest)
+        {
+            return "ok";
+        }
+    }
+}
